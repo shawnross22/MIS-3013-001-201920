2d74d1bfd06957abdc955090240edab06aa4e1b1
ross0012 <ross0012@PCB-LAB360-45>
2020-01-22 12:54:14 -0600

    Created variables for firstName, lastName, favoriteNumber . Also derived crazyNumber.

--- InClassExamples/Variables/Program.cs
+++ InClassExamples/Variables/Program.cs
@@ -11,6 +11,30 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-        }
+            //Create a string named firstName
+            string firstName;
+
+
+
+            //Assign a value to firstName (use your name)
+            firstName = "Shawn";
+
+            //Create an int named favoriteNumber and assign your favorite number to it
+            int favoriteNumber = 22;
+
+            double crazyNumber = 7.6767 * favoriteNumber;
+
+            string lastName = "Ross";
+
+            string fullName = firstName + " " + lastName;
+
+            Console.WriteLine(fullName + " has the favorite number of " + favoriteNumber + " \nwhich equates to a crazy number of " + crazyNumber);
+                                         
+            // MAKE IT DYNAMIC
+                  
+            Console.ReadKey();
+
+    
+            }
     }
 }
