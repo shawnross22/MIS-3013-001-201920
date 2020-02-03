7387c170fcd1bec94c9efc1cb95d1ab8c94e21ff
ross0012 <ross0012@PCB-LAB360-45>
2020-02-03 13:05:46 -0600

    Do while, for loops

---
+++ ConditionalCoinFlipLoop/ConditionalCoinFlipLoop/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalCoinFlipLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (choice != flip[fIndex])
+            {
+
+                Console.WriteLine("Pick heads or tails: >>");
+                string choice = Console.ReadLine().ToLower();
+
+
+                Random rnd = new Random();
+                string[] flip = { "heads", "tails" };
+
+                int fIndex = rnd.Next(flip.Length);
+
+                Console.WriteLine("The flip was {0}", flip[fIndex]);
+
+
+
+                if (choice == flip[fIndex])
+                {
+                    Console.WriteLine("Your guess was right!");
+                }
+                else
+                {
+                    Console.WriteLine("Your guess was wrong :(");
+                }
+
+                const string developerName = "Shawn Ross";
+                Console.WriteLine(developerName);
+            }
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
