5c4935c911214640cb1d76983beb4e5416b1ee08
ross0012 <ross0012@PCB-LAB360-45>
2020-02-19 12:59:59 -0600

    Finished the collections participation worked with Stephanie Fox and Matt Boensch.

---
+++ Collections Participation/Collections Participation/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections_Participation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            string answer;
+            List<double> score = new List<double>();
+            do
+            {
+                Console.WriteLine("Enter your test grades.");
+                double inputnum = Convert.ToDouble(Console.ReadLine());
+                score.Add(inputnum);
+                sum += inputnum;
+
+
+                Console.WriteLine("Do you have another score to add? (y,n) ");
+                answer = Console.ReadLine();
+
+
+            } while (answer.ToLower() == "y");
+
+            
+            double max = score[0];
+            double min = score[0];
+            double average= sum/score.Count;
+
+            for (int i = 0; i < score.Count; i++)
+            {   
+                bool a = score[i] < min; 
+                bool b = score[i] > max;
+                if (b == true)
+                {
+                    max = score[i];
+                }
+                if (a == true)
+                {
+                    min = score[i];
+                }
+            }
+           
+
+            Console.WriteLine("Your max exam score is " + max + ".");
+            Console.WriteLine("Your min exam score is " + min + ".");
+            Console.WriteLine("Your avg exam score is " + average + ".");
+
+            Console.ReadKey();
+        }
+    }
+}
