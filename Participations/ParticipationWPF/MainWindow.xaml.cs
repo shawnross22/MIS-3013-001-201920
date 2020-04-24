a6a29c71e89445a6a87b2cf1268a2cf77b839071
Shawn Ross <Shawn.M.Ross-1@ou.edu>
2020-04-24 16:44:15 -0500

    Finished WPF-2 Participation

---
+++ Participations/ParticipationWPF/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ParticipationWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcess_Click(object sender, RoutedEventArgs e)
+        {
+            //completed on Shawn Ross's computer as mine does not run VirtualStudio
+            string name = txtName.Text;
+            string favColor = txtColor.Text;
+            string myFavColor = "green";
+            if (favColor.ToLower() == myFavColor)
+            {
+                MessageBox.Show("Wow! We have the same favorite color!");
+            }
+            else
+            {
+                MessageBox.Show($"{name} that is really cool, I never would have expected your favorite color to be {favColor}");
+               
+
+            }
+        }
+    }
+}
---
+++ Participations/WPF-2 Participation/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_2_Participation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void rbtnOdd_Checked(object sender, RoutedEventArgs e)
+        {
+            string numberAsText = txtNumber.Text;
+            int number = Convert.ToInt32(numberAsText);
+
+            for (int i = 0; i <= number; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    lstNumbers.Items.Add(i);
+                }
+            }
+        }
+
+        private void rbtnEven_Checked(object sender, RoutedEventArgs e)
+        {
+            string numberAsText = txtNumber.Text;
+            int number = Convert.ToInt32(numberAsText);
+
+            for (int i = 0; i <= number; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    lstNumbers.Items.Add(i);
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtNumber.Clear();
+            lstNumbers.Items.Clear();
+            txtNumber.Focus();
+            rbtnOdd.IsChecked = false;
+            rbtnEven.IsChecked = false;
+        }
+    }
+}
